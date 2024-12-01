efbcdc32085e666459b4d26fe25445bc745a6e97
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-11-30 19:47:35 -0600

    second commit

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -6,46 +6,54 @@
  */
 
 import * as React from "react"
+import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import styled from "styled-components"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Content = styled.div`
+margin:0 auto;
+max-width:var(--size-content);
+padding:var(--size-gutter);
 
-  return (
+`
+
+const Layout = ({children})=>{
+  const data = useStaticQuery(
+    graphql`
+    query SiteTitleQuery{
+    site{
+    siteMetadata{
+    title
+    }
+    }}
+    
+    `
+  )
+
+  return(
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+    <Header siteTitle={data.site.siteMetadata?.title || `Title`}/>
+    <Content>
+      <main>{children}</main>
+      <footer
+      style={{
+        marginTop:`var(--space-5)`,
+        fontSize:`var(--font-sm)`,
+        
+      }}>
+        {new Date().getFullYear()} &middot; Built width
+        {``}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </Content>
     </>
   )
+}
+
+Layout.PropTypes={
+  children:PropTypes.node.isRequired
 }
 
 export default Layout
