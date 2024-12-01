55f60d28d47dfcdf3999f6dc3ccb7e94df56ac86
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-11-30 20:01:37 -0600

    done

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -5,55 +5,54 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import styled from "styled-components"
-import Header from "./header"
-import "./layout.css"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import styled, { ThemeProvider } from "styled-components";
+import Header from "./header";
+import "./layout.css";
+import { Gray } from "./themes/Gray";
 
 const Content = styled.div`
-margin:0 auto;
-max-width:var(--size-content);
-padding:var(--size-gutter);
+  margin: 0 auto;
+  max-width: var(--size-content);
+  padding: var(--size-gutter);
+`;
 
-`
-
-const Layout = ({children})=>{
+const Layout = ({ children }) => {
   const data = useStaticQuery(
     graphql`
-    query SiteTitleQuery{
-    site{
-    siteMetadata{
-    title
-    }
-    }}
-    
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
     `
-  )
+  );
 
-  return(
-    <>
-    <Header siteTitle={data.site.siteMetadata?.title || `Title`}/>
-    <Content>
-      <main>{children}</main>
-      <footer
-      style={{
-        marginTop:`var(--space-5)`,
-        fontSize:`var(--font-sm)`,
-        
-      }}>
-        {new Date().getFullYear()} &middot; Built width
-        {``}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </Content>
-    </>
-  )
-}
+  return (
+    <ThemeProvider theme={Gray}>
+      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
+      <Content>
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `var(--space-5)`,
+            fontSize: `var(--font-sm)`,
+          }}
+        >
+          {new Date().getFullYear()} &middot; Built with {``}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </Content>
+    </ThemeProvider>
+  );
+};
 
-Layout.PropTypes={
-  children:PropTypes.node.isRequired
-}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
