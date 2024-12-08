2885d45df1c5977367888e39b86e65566be78bf3
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-12-07 21:50:35 -0600

    done

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from '../components/layout';
+import Layout from "../components/layout";
 
 const BlogPost = ({ data }) => {
-    const { title, body } = data.contentfulBlogPost;
+  const { title, body } = data.contentfulPost;
 
-    return (
-        <Layout>
-            <h1>{title}</h1>
-            <div
-                dangerouslySetInnerHTML={{
-                    __html: body.childMarkdownRemark.html,
-                }}
-            ></div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <div>
+        {body?.childMarkdownRemark?.html ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: body.childMarkdownRemark.html,
+            }}
+          ></div>
+        ) : (
+          <p>Content not available.</p>
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-    query blogPostQuery($slug: String!) {
-        contentfulBlogPost(slug: { eq: $slug }) {
-            title
-            slug
-            body {
-                childMarkdownRemark {
-                    html
-                }
-            }
+  query blogPostQuery($slug: String!) {
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      slug
+      body {
+        childMarkdownRemark {
+          html
         }
+      }
     }
+  }
 `;
